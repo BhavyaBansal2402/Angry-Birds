81ebb7e8e3c6a4241fab50474dd0d2b076de6538
BhavyaBansal2402 <72801817+BhavyaBansal2402@users.noreply.github.com>
2020-11-10 18:29:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,9 +7,10 @@ var box1, box2, box3, box4, box5;
 var ground;
 var pig1,pig2;
 var log1, log2,log3, log4;
+var bird;
 
 function setup(){
-    var canvas = createCanvas(1200,600);
+    var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
@@ -21,14 +22,16 @@ function setup(){
     pig1 = new Piggy(810, 350);
     log1 = new Log(810, 260, 300, PI/2);
 
-  box3 = new Box(700,240,70,70);
+    box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig2 = new Piggy(810, 220);
     log2 = new Log(810, 180, 300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log3 = new Log(730, 120, 150, PI/5);
-    log4 = new Log(870, 120, 150, -PI/5);
+    log3 = new Log(730, 120, 150, PI/7);
+    log4 = new Log(870, 120, 150, -PI/7);
+
+    bird = new Bird(200, 200);
 }
 
 function draw(){
@@ -50,6 +53,6 @@ function draw(){
     box5.display();
     log3.display();
     log4.display();
-
+    bird.display();
 }
 
